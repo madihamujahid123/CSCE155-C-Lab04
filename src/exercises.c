38d5b4e91db7ae0a4944dad02827aecc74d4bcc0
Madiha Mujahid <madihamujahid96@gmail.com>
2023-10-22 19:44:59 +0500

    lab4 qs4

--- src/exercises.c
+++ src/exercises.c
@@ -55,6 +55,15 @@ int main(int argc, char **argv) {
 
   //4. Print squares of numbers 1 thru n separated by a comma
   //   except for the last one; example: 1, 4, 9, 16, 25
+  for(int i=1;i<=n;i++)
+  {
+    printf("%d",i*i);
+    if(i!=n)
+  {
+    printf(",")
+  }
+  }
+  printf("\n");
 
   //5. Compute the summation of numbers 1 thru n and print
   //   the result; example: 1 + 2 + 3 + 4 + 5 = 15
